0c6633c2910cb945e0e6c3df4942daa10d981c74
lucnguyendinh <locpuntr@gmail.com>
2023-11-06 16:52:20 +0700

    Page

--- Register/index.js
+++ Register/index.js
@@ -13,13 +13,18 @@ const Register = ({ user }) => {
     const handleSubmit = (e) => {
         e.preventDefault()
         const registerUser = async () => {
+            try {
                 const newUser = {
                     username,
                     email,
                     password,
+                }
                 const res = await axios.post('/auth/register', newUser)
                 console.log(res.data)
                 navigate('/login')
+            } catch (error) {
+                console.log(error)
+            }
         }
         registerUser()
     }
@@ -44,6 +49,8 @@ const Register = ({ user }) => {
                         <input
                             type="password"
                             placeholder="password"
+                            onChange={(e) => setPassword(e.target.value)}
+                            value={password}
                         />
                     </div>
                     <button className="register">Register</button>
